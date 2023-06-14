88b8a129b21a4ca449a325786d4e082c4db0a4b8
Yann Le Doaré <yann@linuxconsole.org>
2023-06-14 12:58:26 +0000

    build images

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
+EXPORT_IMAGES=$PWD
+[ -e $HOME/export-images.ini ] && source  $HOME/export-images.ini
+
 ls | while read image
 do
   test -d $image || continue
   cd $image
   echo "Build $image"
-  	[ ! -e $image.img ] && docker build -t $image .
-	exit 1
+  [ ! -e $EXPORT_IMAGES/$image.img ] && docker build -t $image . && docker save -o $EXPORT_IMAGES/$image.img $image
   cd ..
 done
